bcf6c120b2b2e41a81ac5891be3f0b0a448f6361
Peter Weissig <peterweissig@github.com>
2021-03-24 22:12:08 +0100

    fixed sourcing   + escaping all paths   + using "source" instead of "."

--- bashrc.sh
+++ bashrc.sh
@@ -28,66 +28,66 @@ export MASTER_PATH_WORKSPACE="$(realpath "${MASTER_PATH_SCRIPT}../../.." )/"
 
 
 #***************************[repository]**************************************
-# 2021 01 10
+# 2021 03 24
 
-. ${MASTER_PATH_SCRIPT}scripts/repository.sh
+source "${MASTER_PATH_SCRIPT}scripts/repository.sh"
 
 if [ "${REPO_BASH_REPO[0]}" != "" ] && \
   [ -d "${REPO_BASH_REPO[0]}" ]; then
     export REPO_PATH_WORKSPACE="${MASTER_PATH_WORKSPACE}"
-    . ${REPO_BASH_REPO[0]}bashrc.sh
+    source "${REPO_BASH_REPO[0]}bashrc.sh"
 fi
 
 
 
 #***************************[repo config]*************************************
-# 2021 01 10
+# 2021 03 24
 
 if [ "${REPO_BASH_CONFIG[0]}" != "" ] && \
   [ -d "${REPO_BASH_CONFIG[0]}" ]; then
-    . ${REPO_BASH_CONFIG[0]}bashrc.sh
+    source "${REPO_BASH_CONFIG[0]}bashrc.sh"
 fi
 
 
 
 #***************************[repo network]************************************
-# 2018 11 30
+# 2021 03 24
 
 if [ "${REPO_BASH_NETWORK[0]}" != "" ] && \
   [ -d "${REPO_BASH_NETWORK[0]}" ]; then
-    . ${REPO_BASH_NETWORK[0]}bashrc.sh
+    source "${REPO_BASH_NETWORK[0]}bashrc.sh"
 fi
 
 
 
 #***************************[repo multimedia]*********************************
-# 2018 11 30
+# 2021 03 24
 
 if [ "${REPO_BASH_MULTIMEDIA[0]}" != "" ] && \
   [ -d "${REPO_BASH_MULTIMEDIA[0]}" ]; then
-    . ${REPO_BASH_MULTIMEDIA[0]}bashrc.sh
+    source "${REPO_BASH_MULTIMEDIA[0]}bashrc.sh"
 fi
 
 
 
 #***************************[repo file]***************************************
-# 2018 11 30
+# 2021 03 24
 
 if [ "${REPO_BASH_FILE[0]}" != "" ] && \
   [ -d "${REPO_BASH_FILE[0]}" ]; then
-    . ${REPO_BASH_FILE[0]}bashrc.sh
+    source "${REPO_BASH_FILE[0]}bashrc.sh"
 fi
 
 
 
 #***************************[help]********************************************
-# 2021 01 10
+# 2021 03 24
 
-. ${MASTER_PATH_SCRIPT}scripts/help.sh
+source "${MASTER_PATH_SCRIPT}scripts/help.sh"
 
 
 
 #***************************[backwards compatibility]*************************
-# 2021 01 03
+# 2021 03 24
 
-. ${MASTER_PATH_SCRIPT}scripts/backward.sh
+source "${MASTER_PATH_SCRIPT}scripts/backward.sh"
