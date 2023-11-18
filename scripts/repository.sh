ec1e2e18e996e80dab10dc7a54001adccb2d49fa
Peter Weissig <peterweissig@github.com>
2023-11-19 00:26:19 +0100

    replaced aliases with functions

--- scripts/repository.sh
+++ scripts/repository.sh
@@ -14,7 +14,7 @@ export REPO_ROOT_GITHUB_URL="https://github.com/peterweissig/"
 
 
 #***************************[bash]********************************************
-# 2019 12 01
+# 2023 11 18
 
 # paths
 # note: this is already set in file bash/repo/list.sh
@@ -26,29 +26,33 @@ export REPO_BASH_MASTER_PATH="${REPO_BASH_PATH}master/"
 export REPO_BASH_REPO=("${REPO_BASH_PATH}repo/" \
   "${REPO_ROOT_GITHUB_URL}bash_repo.git")
 
-alias git_clone_bash_repo="_repo_git_clone ${REPO_BASH_REPO[*]}"
-alias git_pull_bash_repo="_repo_git_pull \"${REPO_BASH_REPO[0]}\""
-alias git_push_bash_repo="_repo_git_push \"${REPO_BASH_REPO[0]}\""
-alias git_status_bash_repo="_repo_git_st \"${REPO_BASH_REPO[0]}\""
+function git_clone_bash_repo()  { _repo_git_clone ${REPO_BASH_REPO[*]};  }
+function git_pull_bash_repo()   { _repo_git_pull "${REPO_BASH_REPO[0]}"; }
+function git_push_bash_repo()   { _repo_git_push "${REPO_BASH_REPO[0]}"; }
+function git_status_bash_repo() { _repo_git_st   "${REPO_BASH_REPO[0]}"; }
 
 # main scripts
 # note: this is already set in file bash/repo/list.sh
 export REPO_BASH_MASTER_BASHONLY=("${REPO_BASH_MASTER_PATH}bash_only/" \
   "${REPO_ROOT_GITHUB_URL}bash_master_simple.git")
 
-alias git_clone_bash_master_simple="_repo_git_clone \
-  ${REPO_BASH_MASTER_BASHONLY[*]}"
-alias git_pull_bash_master_simple="_repo_git_pull \
-  \"${REPO_BASH_MASTER_BASHONLY[0]}\""
-alias git_push_bash_master_simple="_repo_git_push \
-  \"${REPO_BASH_MASTER_BASHONLY[0]}\""
-alias git_status_bash_master_simple="_repo_git_st \
-  \"${REPO_BASH_MASTER_BASHONLY[0]}\""
+function git_clone_bash_master_simple() {
+    _repo_git_clone ${REPO_BASH_MASTER_BASHONLY[*]}
+}
+function git_pull_bash_master_simple() {
+    _repo_git_pull "${REPO_BASH_MASTER_BASHONLY[0]}"
+}
+function git_push_bash_master_simple() {
+    _repo_git_push "${REPO_BASH_MASTER_BASHONLY[0]}"
+}
+function git_status_bash_master_simple() {
+    _repo_git_st "${REPO_BASH_MASTER_BASHONLY[0]}"
+}
 
 
 #***************************[global update and stat]**************************
-# 2018 11 30
+# 2023 11 18
 
-alias master_repo_update="repo_pull_all"
-alias master_repo_status="repo_status_all"
+function master_repo_update() { repo_pull_all;  }
+function master_repo_status() { repo_status_all; }
 
